7730d548e6cad712087b27a538371b057a97272e
Khanh Tran <katetrannbkhanh@gmail.com>
2023-08-13 02:10:28 -0700

    commit tree 1cd106693fb564cb100a55de42986ad832f3b0c4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,26 @@ class ArticlesController < ApplicationController
     render json: article
   end
 
+  def create
+    user = User.find_by(username: params[:username])
+    session[:user_id] = user.id
+    render json: user
+  end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: {error: "Not authorized"}, status: :unauthorized
+    end
+  end
+
   private
 
   def record_not_found
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "articles#create"
+  delete "/logout", to: "articles#destroy"
+  get "/me", to: "articles#show"
 end
